ecb101c8118194307af63ed39ca1829b75ebd96f
wandin <wanderson.lopes@outlook.com>
2024-03-08 20:17:51 -0300

    using another math lib + fixin bugs in cohesion, need test separation

--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
 -- Importing vector's library
-local vector = require "libraries/game-tools.vector"
+local vector = require "libraries/hump.vector"
 
 -- Window's settings
 local windowWidth = 1280
@@ -10,23 +10,31 @@ love.window.setMode(windowWidth, windowHeight)
 
 -- Creating a new boid
 local function createBoid()
-    local startPosition = vector(math.random(0, windowWidth), math.random(0,windowHeight))
+    local startPosition = vector.new(math.random(0, windowWidth), math.random(0,windowHeight))
     
+    local magnitude = love.math.random(2, 100)   -- Magnitude aleatória entre 2 e 4
+    --local direction = vector.new(love.math.random(-1, 1), love.math.random(-1, 1))
+
+    local direction = vector.new(love.math.random() * 2 - 1, love.math.random() * 2 - 1)
+    direction:normalizeInplace()
+    local sumvelocity = direction * magnitude  -- Ajustar magnitude da velocidade
+
+
     return{
+        colour = {math.random(0.2,1), math.random(0.2,1), math.random(0.2,1)},
         position = startPosition,
-        velocity = vector(love.math.random(50, 200), love.math.random(50, 200)),
-        acceleration = vector.ZERO,         -- Top speed
-        colour = {math.random(0,1), math.random(0,1), math.random(0,1)},
-        maxForce = 0.2,
-        maxSpeed = 200
+        velocity = sumvelocity,
+        acceleration = vector.new(0,0),         
+        maxForce = 1,
+        maxSpeed = 100
     }
 end
 
 -- Inicialização do jogo
 function love.load()
-    -- add to Lista de boids
+    -- adding boids to array
     boids = {}
-    for i = 1, 50 do
+    for i = 1, 30 do
         boid = table.insert(boids, createBoid())
     end
 end
@@ -43,41 +51,14 @@ end
 
 
 function love.update(dt)
+    cohesion(boids)
 
-    flock(boids)
-
-    -- UPDATE BOID POSITION!
 
     for i, b in pairs(boids) do
-
-        local aligment = align(boids)
-        b.acceleration = aligment
-
-
-        -- Atualizar a posição com base na velocity e no tempo decorrido (dt)
+        -- UPDATE BOID POSITION!
         b.position = b.position + b.velocity * dt
         b.velocity = b.velocity + b.acceleration * dt
-
-      --[[   -- Verificar se o boid atingiu as bordas da tela e ajustar sua posição e velocity se necessário
-        if b.position.x < 0 then
-            b.position.x = 0
-            -- Ajustar o ângulo para apontar para a direita
-            b.velocity.x = math.abs(b.velocity.x)
-        elseif b.position.x > windowWidth then
-            b.position.x = windowWidth
-            -- Ajustar o ângulo para apontar para a esquerda
-            b.velocity.x = -math.abs(b.velocity.x)
-        end
-        if b.position.y < 0 then
-            b.position.y = 0
-            -- Ajustar o ângulo para apontar para baixo
-            b.velocity.y = math.abs(b.velocity.y)
-        elseif b.position.y > windowHeight then
-            b.position.y = windowHeight
-            -- Ajustar o ângulo para apontar para cima
-            b.velocity.y = -math.abs(b.velocity.y)
-        end]]
-
+        b.velocity = b.velocity:trimInplace(b.maxSpeed)
 
        if b.position.x < 0 then
             b.position.x = windowWidth
@@ -90,8 +71,7 @@ function love.update(dt)
             b.position.y = 0
         end
     end    
-    
-    
+    flock(boids) 
 end
 
 
@@ -106,71 +86,93 @@ end
 
 function align(boids)
     local radius = 100
-    local steeringForce = vector.ZERO
+    local steeringForce = vector.new(0,0)
     local totalBoids = 0
 
-    maxSpeed = 0
     for _, boid in pairs(boids) do
-
         for _, otherboid in ipairs(boids) do
-
-            local distance = vector.distanceTo(boid.position, otherboid.position)
-
+            local distance = boid.position:dist(otherboid.position)
             if boid ~= otherboid and distance < radius then
-
                 steeringForce = steeringForce + otherboid.velocity
                 totalBoids = totalBoids + 1
             end
-        end
-   
-        if totalBoids > 0 then
-            steeringForce = steeringForce / totalBoids
-            steeringForce = steeringForce - boid.velocity
-            vector.normalized(steeringForce * math.min(0, boid.maxSpeed, boid.maxForce))
-
         end
     end
+    if totalBoids > 0 then
+        steeringForce = steeringForce / totalBoids
+        steeringForce = steeringForce:normalizeInplace()                -- normalize to set magnitude below
+        steeringForce = steeringForce * boids[1].maxSpeed               -- multiplying by maxSpeed after normalizing (kinda setMag)
+        steeringForce = steeringForce - boids[1].velocity
+        steeringForce = steeringForce:trimInplace(boids[1].maxForce)    -- limiting to maxForce
+    end
     return steeringForce
-
 end
 
-
-function cohesion(boids)
-    local radius = 100
-    local steeringForce = vector.ZERO
+function separation(boids)
+    local radius = 50
+    local steeringForce = vector.new(0,0)
     local totalBoids = 0
 
-    maxSpeed = 0
     for _, boid in pairs(boids) do
-
         for _, otherboid in ipairs(boids) do
-
-            local distance = vector.distanceTo(boid.position, otherboid.position)
-
+            local distance = boid.position:dist(otherboid.position)
             if boid ~= otherboid and distance < radius then
+                local distanceMultiplied = distance * distance
 
-                steeringForce = steeringForce + otherboid.velocity
+                local distanceToOthers = vector.new(0,0)
+                distanceToOthers = distanceToOthers.__sub(boid.position, otherboid.position)
+                distanceToOthers = distanceToOthers / distanceMultiplied
+                steeringForce = steeringForce + distanceToOthers
                 totalBoids = totalBoids + 1
             end
         end
-   
+    end
+    if totalBoids > 0 then
+        steeringForce = steeringForce / totalBoids
+        steeringForce = steeringForce:normalizeInplace()            -- normalize to set magnitude below
+        steeringForce = steeringForce * boids[1].maxSpeed               -- multiplying by maxSpeed after normalizing (kinda setMag)
+
+        steeringForce = steeringForce - boids[1].velocity
+        steeringForce = steeringForce:trimInplace(boids[1].maxForce)    -- limiting to maxForce
+    end
+    return steeringForce
+end
+
+function cohesion(boids)
+    local radius = 500
+    local steeringForce = vector.new(0,0)
+    local totalBoids = 0
+
+    for _, boid in pairs(boids) do
+        for _, otherboid in ipairs(boids) do
+            local distance = boid.position:dist(otherboid.position)
+
+            if boid ~= otherboid and distance < radius then
+                steeringForce = steeringForce + otherboid.position
+                totalBoids = totalBoids + 1
+            end
+        end
+
         if totalBoids > 0 then
             steeringForce = steeringForce / totalBoids
             steeringForce = steeringForce - boid.position
-            vector.normalized(steeringForce * math.min(0, boid.maxSpeed, boid.maxForce))
+            steeringForce = steeringForce:normalizeInplace() * boid.maxSpeed
             steeringForce = steeringForce - boid.velocity
+            steeringForce = steeringForce:trimInplace(boid.maxForce)
+            steeringForce = steeringForce:trimInplace(boid.maxSpeed)
 
+            boid.acceleration = boid.acceleration + steeringForce
         end
     end
     return steeringForce
-
 end
 
 
 function flock(boids)
+    local alignment = align(boids)
+    local cohesion = cohesion(boids)
+    --local separation = separation(boids)
     for _,b in pairs(boids) do
-        local alignment = align(boids)
-        local cohesion = cohesion(boids)
-        b.acceleration = alignment + cohesion
+        b.acceleration = b.acceleration + alignment + cohesion --+ separation
     end
 end
